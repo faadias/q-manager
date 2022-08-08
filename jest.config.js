ca1d70ff9679ed54c79fd9f5336b0b4eaa831da9
faadias <faadias@gmail.com>
2022-08-08 11:44:43 -0300

    Correção para rodar apenas os testes .ts

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  testMatch: ["<rootDir>/test/**/*.spec.ts"],
+  clearMocks: true,
   transform: {
     "^.+\\.ts?$": "ts-jest"
   },
